f925f9fc5843d1a4467c604a12bf1901d90c74cb
josh-miller <joshuae.miller100@gmail.com>
2022-02-20 16:21:05 +0100

    added a homepage

---
+++ utils/fetchApi.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+export const baseUrl = 'https://bayut.p.rapidapi.com';
+
+export const fetchApi = async (url) => {
+  const { data } = await axios.get((url), {
+    headers: {
+      'x-rapidapi-host': 'bayut.p.rapidapi.com',
+      "x-rapidapi-key": "2dd8e8ea38msh09b29c83b63b77ap18a346jsnd63922c98f28",
+    },
+  });
+    
+  return data;
+}
