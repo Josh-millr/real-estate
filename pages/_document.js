4b119a01c2c2dafc4e4bf5ea9a8b4472bb519ada
josh-miller <joshuae.miller100@gmail.com>
2022-02-21 22:24:13 +0100

    added theming configuration

--- pages/_document.js
+++ pages/_document.js
@@ -1,12 +1,13 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 import { ColorModeScript } from '@chakra-ui/react'
+import theme from "../utils/theme"
 
 export default function Document() {
   return (
     <Html>
       <Head />
       <body>
-      <ColorModeScript />
+      <ColorModeScript initialColorMode={theme.config.initialColorMode}/>
         <Main />
         <NextScript />
       </body>
